2795fe9b344d5eb160f1cffde56d38cc1ddbd80a
xiekind <ziekind07@gmail.com>
2020-09-18 17:23:34 +0800

    modified/updated

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+// import { Message } from '../../message';
 
 @Component({
   selector: 'app-contact',
